e6f6ae54cd477e40c56f8002bd4c624b2293c46b
Thomas Pratama <pratama.thomas@gmail.com>
2024-09-12 15:57:19 +0700

    add exercise day 13

---
+++ Day13-exercise.js
@@ -0,0 +1,17 @@
+//Day 13 - Exercise
+//Thursday, 12 September 2024
+
+
+// Question 1 - Food Ordering Process
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class QueueFood {
+    constructor() {
+        
+    }
+}
